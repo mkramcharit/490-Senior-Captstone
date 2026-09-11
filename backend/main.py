b22cd437249d6d3b23d17d1f7ff6034e53615516
Brycen5 <bmparsons@uncg.edu>
2026-09-11 13:22:04 -0400

    Updated Predict to catch bad requests

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 # backend/main.py
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from PIL import Image
+from io import BytesIO
 
 app = FastAPI(title="Trekmark API")
 
@@ -14,6 +16,39 @@ def health_check():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
+
+    # Check that a file was actually provided
+    if not file:
+        raise HTTPException(
+            status_code=400,
+            detail="No file uploaded."
+        )
+
+    if not file.content_type or not file.content_type.startswith("image/"):
+        raise HTTPException(
+            status_code=400,
+            detail="Uploaded file must be an image."
+        )
+
+    contents = await file.read()
+
+    # Checks that the file isn't empty
+    if not contents:
+        raise HTTPException(
+            status_code=400,
+            detail="Uploaded file is empty."
+        )
+
+    # API tries opening the file as an actual image
+    try:
+        image = Image.open(BytesIO(contents))
+        image.verify()
+    except Exception:
+        raise HTTPException(
+            status_code=400,
+            detail="Uploaded file is not a valid image."
+        )
+
     return {
         "filename": file.filename,
         "content_type": file.content_type,
